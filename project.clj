06388e4fe3991d91e7a0cc718db3381f313298e2
Jacek Lach <jacek@lshift.net>
2013-11-21 21:31:03 -0500

    Add travis, cleanup project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.2.11"]
-                 [midje "1.6-beta1"]])
+                 [instaparse "1.2.11"]]
+  :profiles {:dev {:dependencies [[midje "1.6-beta1"]]
+                   :plugins      [[lein-midje "3.0.0"]]}})
